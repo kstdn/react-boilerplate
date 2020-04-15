f2bd15df39a5d02ef8741c5e6e7e232b8736c352
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-04-15 19:17:36 +0300

    Adjust Auth component

--- src/Auth/index.tsx
+++ src/Auth/index.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const AuthFormContainer = styled.div`
-  background: red;
-  padding: 20px;
-`;
 
 export const Authentication = () => {
   return (
-    <AuthFormContainer>
-      Login
-      <input type='text' />
-      <input type='password' />
-    </AuthFormContainer>
+    <div>Auth</div>
   );
 };
